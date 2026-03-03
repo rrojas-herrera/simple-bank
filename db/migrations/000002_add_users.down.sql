bf4c522b58312bd91cb742ce74972db22ae02644
rrojas-herrera <rrojas@imed.cl>
2026-03-03 10:54:36 -0300

    fix migrations

--- db/migrations/000002_add_users.down.sql
+++ db/migrations/000002_add_users.down.sql
@@ -1,5 +1,5 @@
 ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_currency_key";
 
-ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_currency_key";
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_pkey";
 
 DROP TABLE IF EXISTS "users";
